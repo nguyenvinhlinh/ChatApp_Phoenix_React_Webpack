b5286f30d296babd757f5cdcdc5abda8c4de9fb4
nguyen vinh linh <nguyenvinhlinh@localhost.localdomain>
2016-02-29 16:17:57 +0700

    set up the skeleton,

--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,8 @@ path = require("path");
 module.exports = {
   context: path.join(__dirname, "webpack_src"),
   entry: {
-    app: "./js/app.js"
+    app: "./js/app.js",
+    chat: "./js/chat.js"
   },
   output: {
     path: path.join(__dirname, "priv/static/js"),
