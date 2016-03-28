325dedb0d33ba2844877396121c626af360cc13a
nguyen vinh linh <nguyenvinhlinh@localhost.localdomain>
2016-03-28 23:52:00 +0700

    fix bug, the first param of bind is this or null

--- webpack_src/js/chat/components/chat_room_list.jsx
+++ webpack_src/js/chat/components/chat_room_list.jsx
@@ -15,7 +15,7 @@ var ChatRoomRow = React.createClass({
     return(
       <li className={className} role="presentation" >
         <a href="#" data-roomid={this.props.roomid}
-           key={this.props.roomid} onClick={ChatAppActions.click_on_chat_row.bind(this.props.roomid)} >
+           key={this.props.roomid} onClick={ChatAppActions.click_on_chat_row.bind(null, this.props.roomid)} >
           {this.props.roomname}
         </a>
       </li>
