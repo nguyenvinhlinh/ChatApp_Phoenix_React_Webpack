52ce39c8fe96b19f7b3b6a57319c69b55cb9b826
nguyen vinh linh <nguyenvinhlinh@localhost.localdomain>
2016-03-28 21:33:51 +0700

    add store, actions, constants
    update dispatcher

---
+++ webpack_src/js/chat/constants/ChatAppConstants.js
@@ -0,0 +1,8 @@
+var ChatAppConstants = {
+  ChatApp_post_message: "ChatApp_post_message",
+  ChatApp_click_on_chat_row: "ChatApp_click_on_chat_row",
+  ChatApp_update_state_message: "ChatApp_update_state_message",
+  ChatApp_update_state_user_status: "ChatApp_update_state_user_status",
+  ChatApp_insert_state_user_status: "ChatApp_insert_state_user_status"
+}
+export{ChatAppConstants};
